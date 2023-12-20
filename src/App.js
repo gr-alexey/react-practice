821cf1d53165cb67a5fb9823e1a02037ae580fb8
Alex <gryshchenko.oleksii@gmail.com>
2023-12-20 17:17:08 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Заголовок все работает</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    re; turn(
+        <header className="header">
+            <h1>Page Titel</h1>
+            <p>Это вообшще космос Алекс</p>
+        </header>
+    )
+}
+
+export default Header;
